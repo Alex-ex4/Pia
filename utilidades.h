13024a53b75786121a38aa1d551178d9705b518b
Alex-ex4 <lazarosilva2006@gmail.com>
2025-11-05 22:49:07 -0600

    Add Historial struct and update Zona struct

--- utilidades.h
+++ utilidades.h
@@ -7,12 +7,20 @@ typedef struct {
     char nom[50];
     float umbral;
     char ventilador[5];
+    Historial *historiales;
 } Zona;
 
 typedef struct {
     char usuario[50];
     char password[50];
 } Usuario;
+
+typedef struct {
+    int idZona;
+    float temperatura;
+    char fecha[20];
+    char hora[10];
+} Historial;
 
 void escribirArchivo(Zona *zonas, int cont);
 
@@ -22,13 +30,17 @@ void listaZonas();
 
 float generarTemp(int min, int max);
 
-void registrarZona(Zona **zonas, int *cont);
+void registrarZona(Zona **zonas, int *cont, float **tempPredet, int **contHistorial);
 
 void temperaturaActual( Zona **zonas, int *cont);
 
 void limpiar_buffer();
 
 int validar_usuario();
+
+void activarVent(Zona **zonas, int *cont);
+
+void reporte(Zona **zonas, int *cont, int **contHistorial);
 
 //void leer_cadena(const char* mensaje, char* cadena, int longitud);
 
