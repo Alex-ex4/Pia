7173bb3fc5382b5d8e7b6bf7e2df8274cce0b754
epsilon-007 <edsonamaya007@gmail.com>
2025-11-06 20:47:10 -0600

    yo

--- utilidades.c
+++ utilidades.c
@@ -3,118 +3,38 @@
 #include <string.h>
 #include <time.h>
 #include "utilidades.h"
-#define ARCHIVO_USUARIOS "usuarios.bin"
-//#define MAX_NOMBRE 50
 #define ARCHIVO "archivo.bin"
 #define HISTORIAL "historial.csv"
 
-int validar_usuario() {
-    FILE *archivo = fopen(ARCHIVO_USUARIOS, "rb");
-    if (archivo == NULL) {
-        archivo = fopen(ARCHIVO_USUARIOS, "wb");
-        if (archivo != NULL) {
-            Usuario admin = {"Padron", "123456"};
-            fwrite(&admin, sizeof(Usuario), 1, archivo);
-            fclose(archivo);
-        }
-    } else {
-        fclose(archivo);
-    }
 
-    char usuario[50];
-    char password[50];
-
-    printf("=== Bienvenido ===\n");
-    printf("Ingrese usuario: ");
-    fgets(usuario, 50, stdin);
-    usuario[strcspn(usuario, "\n")] = '\0';
-
-    //leer_cadena("", usuario, 50);
-    printf("Ingrese contraseña: ");
-    fgets(password, 50, stdin);
-    password[strcspn(password, "\n")] = '\0';
-    //leer_cadena("", password, 50);
-
-    archivo = fopen(ARCHIVO_USUARIOS, "rb");
-    if (archivo == NULL) {
-        printf("Error: No se pudo abrir el archivo de usuarios.\n");
-        return 0;
-    }
-
-    Usuario user;
-    int encontrado = 0;
-    while (fread(&user, sizeof(Usuario), 1, archivo)) {
-        if (strcmp(user.usuario, usuario) == 0 && strcmp(user.password, password) == 0) {
-            encontrado = 1;
-            break;
-        }
-    }
-    fclose(archivo);
-
-    if (!encontrado) {
-        printf("Error: Usuario o contraseña incorrectos.\n\n");
-    }
-    return encontrado;
+float generarTemp(int min, int max){
+    srand(time(NULL));
+    float temp=min+rand()%(max-min+1);
+    return temp;
 }
 
-void limpiar_buffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
+void registrarZona(Zona **zonas, int *cont){
+    Zona nuevaZona;
+    printf("Ingrese ID de la zona: ");
+    scanf("%d", &nuevaZona.id);
+    printf("Ingrese nombre de la zona: ");
+    fgets(nuevaZona.nom, sizeof(nuevaZona.nom), stdin);
+    nuevaZona.nom[strcspn(nuevaZona.nom, "\n")] = '\0';
+    printf("Ingrese umbral de temperatura: ");
+    scanf("%f", &nuevaZona.umbral);
+    strcpy(nuevaZona.ventilador, "OFF");
 
-/*int leer_entero(const char* mensaje) {
-    int valor;
-    printf("%s", mensaje);
-    while (scanf("%d", &valor) != 1) {
-        printf("Error: Ingrese un número entero válido: ");
-        limpiar_buffer();
-    }
-    limpiar_buffer();
-    return valor;
-}
+    (*cont)++;
+    *zonas=realloc(*zonas, ((*cont)+1)*sizeof(Zona));
+    *zonas[(*cont) - 1] = nuevaZona;
 
-float leer_flotante(const char* mensaje) {
-    float valor;
-    printf("%s", mensaje);
-    while (scanf("%f", &valor) != 1) {
-        printf("Error: Ingrese un número válido: ");
-        limpiar_buffer();
-    }
-    limpiar_buffer();
-    return valor;
-}*/
-
-/*void leer_cadena(const char* mensaje, char* cadena, int longitud) {
-    printf("%s", mensaje);
-    if (fgets(cadena, longitud, stdin) != NULL) {
-        cadena[strcspn(cadena, "\n")] = '\0';
-    }
-}*/
-
-
-
-
-
-
-void escribirArchivo(Zona *zonas, int cont){
-    FILE *archivo = fopen(ARCHIVO, "wb");
-    if (archivo != NULL) {
-        fwrite(zonas, sizeof(Zona), cont, archivo);
-        fclose(archivo);
-        printf("Archivo actualizado exitosamente.\n");
-    } else {
-        printf("Error al abrir el archivo para escribir.\n");
-    }
-}
-
-void agregarArchivo(Zona zona){
     FILE *archivo = fopen(ARCHIVO, "ab");
     if (archivo != NULL) {
-        fwrite(&zona, sizeof(Zona), 1, archivo);
+        fwrite(&nuevaZona, sizeof(Zona), 1, archivo);
         fclose(archivo);
-        printf("Zona agregada al archivo exitosamente.\n");
+        printf("Zona registrada exitosamente.\n");
     } else {
-        printf("Error al abrir el archivo para agregar la zona.\n");
+        printf("Error al abrir el archivo para registrar la zona.\n");
     }
 }
 
@@ -134,48 +54,97 @@ void listaZonas(){
 }
 
 
-float generarTemp(int min, int max){
-    srand(time(NULL));
-    float temp=min+rand()%(max-min+1);
-    return temp;
-}
-
-void registrarZona(Zona **zonas, int *cont){
-    Zona nuevaZona;
-    printf("Ingrese ID de la zona: ");
-    scanf("%d", &nuevaZona.id);
-    getchar();
-
-    printf("Ingrese nombre de la zona: ");
-    fgets(nuevaZona.nom, sizeof(nuevaZona.nom), stdin);
-    nuevaZona.nom[strcspn(nuevaZona.nom, "\n")] = '\0';
-
-    printf("Ingrese umbral de temperatura: ");
-    scanf("%f", &nuevaZona.umbral);
-    strcpy(nuevaZona.ventilador, "OFF");
-
-    (*cont)++;
-    *zonas=realloc(*zonas, ((*cont)+1)*sizeof(Zona));
-    *zonas[(*cont) - 1] = nuevaZona;
-
-    agregarArchivo(nuevaZona);
-}
-
-
-void temperaturaActual( Zona **zonas, int *cont) {
-    int i;
-    if (zonas == NULL || *zonas == NULL || cont == NULL) return;
-    Zona *arr = *zonas;
-    //posible cambio de la linea  62-66 (leer en documentacion)
-    for(i = 0; i < (*cont) ; i++) {
-        int min = (int)(arr[i].umbral - 5);
-        int max = (int)(arr[i].umbral + 5);
-        float temp = generarTemp(min, max);
-        if (temp > arr[i].umbral) strcpy(arr[i].ventilador, "ON");
-        else strcpy(arr[i].ventilador, "OFF");
-
-        printf("Zona: %s\n", arr[i].nom);
-        printf("Temperatura actual: %.2f °C\nEstado del Ventilador: %s\n\n",
-        temp, arr[i].ventilador);
+// Edson 
+Zona* buscarZonaPorNombre(Zona *arr, int cont, char *nombreBuscado) {
+    for(int i = 0; i < cont ; i++) {
+        if(strcmp(arr[i].nom, nombreBuscado) == 0) {
+            return &arr[i];
+        }
     }
+    return NULL;
 }
+
+// Edson
+void actualizarVentilador( Zona *z) {
+    int min = (int)(z->umbral - 4);
+    int max = (int)(z->umbral + 4);
+    float temp = generarTemp(min, max);
+    if ( temp > z->umbral ) 
+        strcpy(z->ventilador, "ON");
+            else 
+        strcpy(z->ventilador, "OFF");
+
+    printf("Zona: %s\n", z->nom);
+    printf("Temperatura actual: %.2f °C\nEstado del Ventilador: %s\n\n",
+            temp, z->ventilador);
+}
+
+// Edson 
+void temperaturaActual( Zona **zonas, int *cont) {
+    if (zonas == NULL || *zonas == NULL || cont == NULL) return;
+
+    int i, opcion;
+    char nombre[50];
+    Zona *arr = *zonas;
+
+    do 
+    {
+        printf("\n1) Zona especifica por nombre.\n2) LIstar todas las zonas\n3)  Volver");
+        printf("Seleccion: ");  
+        scanf("%d", &opcion);
+        
+        switch (opcion)
+        {
+        case 1: 
+        {
+            printf("\tIngresar el nombre de la zona:");
+            scanf("%49s", nombre);
+            
+            Zona *z = buscarZonaPorNombre(arr, *cont, nombre );
+            
+            if( z ) 
+            {
+                printf("Zona encontrada\n");
+                actualizarVentilador(z);
+            } else {
+                printf("Zona no encontrada");
+            }
+            break;
+        }
+        case 2:
+            for(i = 0; i < (*cont) ; i++) 
+            {
+                actualizarVentilador( &arr[i]);
+            }
+        default:
+        printf("Opcion invalida...");
+            break;
+        }
+    }while(opcion != 3);
+}
+
+// Edson
+//Integracion: cambiarUmbral(arr,*cont);
+void cambiarUmbral(Zona *arr, int cont) {
+    if ( arr == NULL || cont <= 0) return;
+
+    char nombre[50];
+    int nuevoUmbral;
+    printf("\tIngresar el nombre de la zona:");
+    scanf("%49s", nombre);
+
+    Zona *z = buscarZonaPorNombre( arr , cont , nombre );
+    if( z == NULL) {
+        printf("Zona no encontrada");
+        return;
+    }
+    printf("> Zona _%s_ encontrada\n", z->nom);
+    printf("\n\tUmbral actual: %d °C", z->umbral);
+    printf("\n\tIngrese el nuevo valor del umbral: ");
+    scanf("%d", &nuevoUmbral);
+
+    z->umbral = nuevoUmbral;
+    printf("> Umbral aztualizado correctamente.\n");
+    printf("\n\tUmbral actual: %d °C", z->umbral);
+    actualizarVentilador(z);
+}   
