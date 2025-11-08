3c6ce649168d1e57b438c1104af4aef1a60205ae
Alex-ex4 <lazarosilva2006@gmail.com>
2025-11-08 11:53:06 -0600

    Ya jala creo

--- utilidades.h
+++ utilidades.h
@@ -1,12 +1,21 @@
 #ifndef UTILIDADES_H
 #define UTILIDADES_H
 
+typedef struct {
+    int idZona;
+    float temperatura;
+    int estado_ventilador; // 1 para ON, 0 para OFF, me sirve mas que dejarlo como char asi que lo dejo asi
+    time_t hora; //time_t es un tipo de dato para almacenar tiempo para que no se confundan plebes
+} Historial;
 
 typedef struct {
     int id;
     char nom[50];
     float umbral;
     char ventilador[5];
+    Historial *historiales;
+    float temp_predet;
+    int cont_historial;
 } Zona;
 
 typedef struct {
@@ -14,22 +23,62 @@ typedef struct {
     char password[50];
 } Usuario;
 
+
+//void leer_cadena(const char* mensaje, char* cadena, int longitud);
+
+
+//Otras funciones
+
+void limpiar_buffer();
+
+float generar_temperatura_aleatoria();
+float generarTemp(int min, int max);
+
+Zona* buscarZonaPorNombre(Zona *arr, int cont, char *nombreBuscado);
+Zona* buscar_zona_por_id(int id_zona, Zona *zona);
+
 void escribirArchivo(Zona *zonas, int cont);
 
 void agregarArchivo(Zona zona);
 
 void listaZonas(); 
 
-float generarTemp(int min, int max);
+void actualizarVentilador( Zona *z);
 
-void registrarZona(Zona **zonas, int *cont);
+void registrar_evento(int id_zona, float temperatura, int estado_ventilador);
 
-void temperaturaActual( Zona **zonas, int *cont);
-
-void limpiar_buffer();
-
+//Usuarios
 int validar_usuario();
 
-//void leer_cadena(const char* mensaje, char* cadena, int longitud);
+//Opcion 1
+void registrarZona(Zona **zonas, int *cont);
+
+//Opcion 2
+void temperaturaActual( Zona **zonas, int *cont);
+
+//Opcion 3
+void activarVent(Zona **zonas, int *cont);
+
+//Opcion 4
+void historial_por_zona();
+void mostrar_historial_zona(int id_zona);
+
+//Opcion 5
+void simular_monitoreo_tiempo_real();
+
+//Opcion 6
+void buscar_eventos_rango();
+
+//Opcion 7
+void reporte(Zona **zonas, int *cont);
+
+//Opcion 8
+void exportar_historial_csv(Zona **zonas, int *cont);
+
+//Opcion 9
+void cambiarUmbral(Zona **arr, int cont);
+
+//Opcion 10
+void restaurar_configuracion_default(Zona **zonas, int *cont);
 
 #endif
